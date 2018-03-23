306a412f5ca580afb8b6b10b6912acaa9e41feb8
sanjeev.a.jha <sanjeev.a.jha@accenture.com>
2018-03-23 13:37:32 +0530

    commit

--- 03-Building Deep Neural Network/ex3.py
+++ 03-Building Deep Neural Network/ex3.py
@@ -1,8 +1,4 @@
 import numpy as np
-from sklearn.datasets import make_circles,make_blobs, make_moons, make_gaussian_quantiles
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegressionCV
-from sklearn.metrics import accuracy_score
 
 def sigmoid(Z):
     A =  1/(1+np.exp(-Z))
@@ -30,7 +26,7 @@ def relu_backward(dA, cache):
     return dZ
 
 def initialize_parameters(n_x, n_h, n_y):
-    np.random.seed(2)
+    np.random.seed(1)
     W1 = np.random.randn(n_h, n_x) * 0.01
     b1 = np.zeros((n_h, 1))
     W2 = np.random.randn(n_y, n_h) * 0.01
@@ -43,6 +39,186 @@ def initialize_parameters(n_x, n_h, n_y):
     parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
     return parameters
 
+def initialize_parameters_deep(layer_dims):
+    np.random.seed(3)
+    parameters = {}
+    L = len(layer_dims)
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * 0.01
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+        assert (parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
+        assert (parameters['b' + str(l)].shape == (layer_dims[l], 1))
+    return parameters
+
+def linear_forward(A, W, b):
+    Z = np.dot(W, A) + b
+    assert (Z.shape == (W.shape[0], A.shape[1]))
+    cache = (A, W, b)
+    return Z, cache
+
+def linear_forward_test_case():
+    np.random.seed(1)
+    A = np.random.randn(3, 2)
+    W = np.random.randn(1, 3)
+    b = np.random.randn(1, 1)
+    return A, W, b
+
+def linear_activation_forward(A_prev, W, b, activation):
+    if activation == "sigmoid":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = sigmoid(Z)
+    elif activation == "relu":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = relu(Z)
+    assert (A.shape == (W.shape[0], A_prev.shape[1]))
+    cache = (linear_cache, activation_cache)
+    return A, cache
+
+def linear_activation_forward_test_case():
+    np.random.seed(2)
+    A_prev = np.random.randn(3,2)
+    W = np.random.randn(1,3)
+    b = np.random.randn(1,1)
+    return A_prev, W, b
+
+def L_model_forward(X, parameters):
+    caches = []
+    A = X
+    L = len(parameters) // 2
+    for l in range(1, L):
+        A_prev = A
+        A, cache = linear_activation_forward(A_prev, parameters["W" + str(l)], parameters["b" + str(l)], activation='relu')
+        caches.append(cache)
+    AL, cache = linear_activation_forward(A,parameters["W" + str(L)], parameters["b" + str(L)], activation='sigmoid')
+    caches.append(cache)
+    assert (AL.shape == (1, X.shape[1]))
+    return AL, caches
+
+def L_model_forward_test_case():
+    np.random.seed(1)
+    X = np.random.randn(4, 2)
+    W1 = np.random.randn(3, 4)
+    b1 = np.random.randn(3, 1)
+    W2 = np.random.randn(1, 3)
+    b2 = np.random.randn(1, 1)
+    parameters = {"W1": W1,"b1": b1,"W2": W2,"b2": b2}
+    return X, parameters
+
+def compute_cost(AL, Y):
+    m = Y.shape[1]
+    cost = (-1./ m) * np.sum(np.multiply(Y, np.log(AL)) + np.multiply((1-Y), np.log( 1-AL)))
+    cost = np.squeeze(cost)
+    assert (cost.shape == ())
+    return cost
+
+def compute_cost_test_case():
+    Y = np.asarray([[1, 1, 1]])
+    aL = np.array([[.8, .9, 0.4]])
+    return Y, aL
+
+def linear_backward(dZ, cache):
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+    dW = (1./m) * np.dot(dZ, cache[0].T)
+    db = (1./m) * np.sum(dZ, axis=1, keepdims=True)
+    dA_prev = np.dot(cache[1].T, dZ)
+    assert (dA_prev.shape == A_prev.shape)
+    assert (dW.shape == W.shape)
+    assert (db.shape == b.shape)
+    return dA_prev, dW, db
+
+def linear_backward_test_case():
+    np.random.seed(1)
+    dZ = np.random.randn(1,2)
+    A = np.random.randn(3,2)
+    W = np.random.randn(1,3)
+    b = np.random.randn(1,1)
+    linear_cache = (A, W, b)
+    return dZ, linear_cache
+
+def linear_activation_backward(dA, cache, activation):
+    linear_cache, activation_cache = cache
+    if activation == "relu":
+        dZ = relu_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+    elif activation == "sigmoid":
+        dZ = sigmoid_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+    return dA_prev, dW, db
+
+
+def linear_activation_backward_test_case():
+    np.random.seed(2)
+    dA = np.random.randn(1, 2)
+    A = np.random.randn(3, 2)
+    W = np.random.randn(1, 3)
+    b = np.random.randn(1, 1)
+    Z = np.random.randn(1, 2)
+    linear_cache = (A, W, b)
+    activation_cache = Z
+    linear_activation_cache = (linear_cache, activation_cache)
+    return dA, linear_activation_cache
+
+def L_model_backward(AL, Y, caches):
+    grads = {}
+    L = len(caches)
+    m = AL.shape[1]
+    Y = Y.reshape(AL.shape)
+    dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+    current_cache = caches[L-1]
+    grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL, current_cache, activation="sigmoid")
+    for l in reversed(range(L - 1)):
+        current_cache = caches[l]
+        dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA" + str(l + 2)], current_cache, activation="relu")
+        grads["dA" + str(l + 1)] = dA_prev_temp
+        grads["dW" + str(l + 1)] = dW_temp
+        grads["db" + str(l + 1)] = db_temp
+    return grads
+
+def L_model_backward_test_case():
+    np.random.seed(3)
+    AL = np.random.randn(1, 2)
+    Y = np.array([[1, 0]])
+
+    A1 = np.random.randn(4,2)
+    W1 = np.random.randn(3,4)
+    b1 = np.random.randn(3,1)
+    Z1 = np.random.randn(3,2)
+    linear_cache_activation_1 = ((A1, W1, b1), Z1)
+
+    A2 = np.random.randn(3,2)
+    W2 = np.random.randn(1,3)
+    b2 = np.random.randn(1,1)
+    Z2 = np.random.randn(1,2)
+    linear_cache_activation_2 = ( (A2, W2, b2), Z2)
+
+    caches = (linear_cache_activation_1, linear_cache_activation_2)
+    return AL, Y, caches
+
+def update_parameters(parameters, grads, learning_rate):
+    L = len(parameters) // 2
+    for l in range(L):
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * grads["dW" + str(l + 1)]
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * grads["db" + str(l + 1)]
+    return parameters
+
+
+def update_parameters_test_case():
+    np.random.seed(2)
+    W1 = np.random.randn(3, 4)
+    b1 = np.random.randn(3, 1)
+    W2 = np.random.randn(1, 3)
+    b2 = np.random.randn(1, 1)
+    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
+
+    np.random.seed(3)
+    dW1 = np.random.randn(3, 4)
+    db1 = np.random.randn(3, 1)
+    dW2 = np.random.randn(1, 3)
+    db2 = np.random.randn(1, 1)
+    grads = {"dW1": dW1, "db1": db1, "dW2": dW2, "db2": db2}
+    return parameters, grads
+
 if __name__ == "__main__":
 
     parameters = initialize_parameters(3, 2, 1)
@@ -50,3 +226,54 @@ if __name__ == "__main__":
     print("b1 = " + str(parameters["b1"]))
     print("W2 = " + str(parameters["W2"]))
     print("b2 = " + str(parameters["b2"]))
+
+    parameters = initialize_parameters_deep([5, 4, 3])
+    print("W1 = " + str(parameters["W1"]))
+    print("b1 = " + str(parameters["b1"]))
+    print("W2 = " + str(parameters["W2"]))
+    print("b2 = " + str(parameters["b2"]))
+
+    A, W, b = linear_forward_test_case()
+    Z, linear_cache = linear_forward(A, W, b)
+    print("Z = " + str(Z))
+
+    A_prev, W, b = linear_activation_forward_test_case()
+    A, linear_activation_cache = linear_activation_forward(A_prev, W, b, activation="sigmoid")
+    print("With sigmoid: A = " + str(A))
+    A, linear_activation_cache = linear_activation_forward(A_prev, W, b, activation="relu")
+    print("With ReLU: A = " + str(A))
+
+    X, parameters = L_model_forward_test_case()
+    AL, caches = L_model_forward(X, parameters)
+    print("AL = " + str(AL))
+    print("Length of caches list = " + str(len(caches)))
+
+    Y, AL = compute_cost_test_case()
+    print("cost = " + str(compute_cost(AL, Y)))
+
+    dZ, linear_cache = linear_backward_test_case()
+    dA_prev, dW, db = linear_backward(dZ, linear_cache)
+    print("dA_prev = " + str(dA_prev))
+    print("dW = " + str(dW))
+    print("db = " + str(db))
+
+    dAL, linear_activation_cache = linear_activation_backward_test_case()
+    dA_prev, dW, db = linear_activation_backward(dAL, linear_activation_cache, activation="sigmoid")
+    print("sigmoid:")
+    print("dA_prev = " + str(dA_prev))
+    print("dW = " + str(dW))
+    print("db = " + str(db) + "\n")
+    dA_prev, dW, db = linear_activation_backward(dAL, linear_activation_cache, activation="relu")
+    print("relu:")
+    print("dA_prev = " + str(dA_prev))
+    print("dW = " + str(dW))
+    print("db = " + str(db))
+
+    AL, Y_assess, caches = L_model_backward_test_case()
+    grads = L_model_backward(AL, Y_assess, caches)
+    print("dW1 = " + str(grads["dW1"]))
+    print("db1 = " + str(grads["db1"]))
+    print("dA1 = " + str(grads["dA1"]))
+
+    parameters, grads = update_parameters_test_case()
+    parameters = update_parameters(parameters, grads, 0.1)
