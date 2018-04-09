d5d5ed4f769df823978d5ea0076fcc90d73fa13f
sanjeev.a.jha <sanjeev.a.jha@accenture.com>
2018-04-09 19:46:18 +0530

    commit

--- 04-Deep Neural Network Application/ex4.py
+++ 04-Deep Neural Network Application/ex4.py
@@ -256,6 +256,7 @@ def test_with_own_image():
     plt.imshow(image)
     print("y = " + str(np.squeeze(my_predicted_image)) + ", your L-layer model predicts a \"" + classes[
         int(np.squeeze(my_predicted_image)),].decode("utf-8") + "\" picture.")
+
 if __name__ == "__main__":
     train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
 
